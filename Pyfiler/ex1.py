c26bcc300d12568323b64a6b1bf721ea768d2388
martin10 <martin10@student.uia.no>
2013-09-10 20:57:16 +0200

    My Current Python files
    Going through "Learing Python the hard way." Will upload all my python
    files here.

---
+++ Pyfiler/ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "this is fun"
+print 'Yay! Printing.'
+print "i'd much rather you 'not'."
+print 'i "said" do not touch this.'
