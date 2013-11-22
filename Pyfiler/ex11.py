8a8f3369dfce87bce656398ad63f745c4509e876
martin10 <martin10@student.uia.no>
2013-11-22 09:53:14 +0100

    Update ex11.py

--- Pyfiler/ex11.py
+++ Pyfiler/ex11.py
@@ -1,3 +1,4 @@
+#A script that take input from a user.
 print "How old are you?",
 age = raw_input()
 print "How tall are you?",
@@ -5,4 +6,5 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
+#Prints the output, including the raw input(variable) fromm the user.
 print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
