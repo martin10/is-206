59c919444cefd78a7e9eda5d3152c4aa1422d63b
martin10 <martin10@student.uia.no>
2013-11-22 10:03:03 +0100

    Update ex7.py

--- Pyfiler/ex7.py
+++ Pyfiler/ex7.py
@@ -1,7 +1,7 @@
 print "Mary had a little lamb."
 print "Its fleece was what as %s." % 'snow'
 print "And everywhere that Mary went."
-print "." * 10 # what 'd that do? 
+print "." * 10 # printing ten dots. 
 
 end1 = "C"
 end2 = "h"
@@ -16,8 +16,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# Watch that comma at the end. Try removing it to see what happens
-# I already know, it will make an error saying that my code is wrong
-
+#Printing out Cheese Burger.  
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12 
