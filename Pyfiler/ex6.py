4b918ee1aeca17aa84a0bf8b10e71fed219bab21
martin10 <martin10@student.uia.no>
2013-11-22 10:01:15 +0100

    Update ex6.py

--- Pyfiler/ex6.py
+++ Pyfiler/ex6.py
@@ -1,15 +1,19 @@
 # -- coding: utf-8 --
+#Telling a joke. 
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
 
+#printing the joke with variables. 
 print x
 print y
 
+#printing it seperatly, with new variables.
 print "I said: %r." % x
 print "I also said: '%s" % y
 
+#Confirming that this joke is lame ;) 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 
@@ -18,4 +22,5 @@ print joke_evaluation % hilarious
 w = "this is the left side of..."
 e = " a string with right side."
 
+#Adding two strings. Printing them as one. 
 print w + e 
